00197426bdf9b1ce8de187d13fe0c292545cdbde
AzothGuyx <cristianxapata@gmail.com>
2020-04-26 15:49:59 -0500

    primer commit

---
+++ src/main/java/com/proyecto/app/ManquilingoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManquilingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManquilingoApplication.class, args);
+	}
+
+}
